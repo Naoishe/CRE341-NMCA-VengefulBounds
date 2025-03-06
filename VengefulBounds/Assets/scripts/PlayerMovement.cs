8b632ecbb3dfed04c2e84a494e955012866ca1aa
Naoishe <McAleese-N6@ulster.ac.uk>
2025-03-06 15:42:06 +0000

    Push1 - basics
    Procedurally generated terrains with trees (trees not procedurally generated), basic player movement with perspective cam

---
+++ VengefulBounds/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,220 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    delegate void PlayerDelegate();
+    PlayerDelegate playerDelegate;
+
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float sprintSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float airMultiplier;
+
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    [SerializeField] private bool grounded;
+    private float primaryY;
+    private float secondaryY;
+    private bool repeatGCloop;
+    private bool isOnStairs;
+
+    public bool isWalking = false;
+
+    public Transform orientation;
+
+    //public Animator anim;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        this.transform.position = new Vector3(-0.5f, -3, -1);
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        repeatGCloop = true;
+
+        playerDelegate += MyInput;
+        playerDelegate += SpeedControl;
+    }
+
+
+    private void Update()
+    {
+
+        if (repeatGCloop)
+        {
+            StartCoroutine(testGroundCheck());
+        }
+        playerDelegate();
+        //handle drag
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+
+        }
+        else
+        {
+            rb.drag = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(jumpKey) && grounded)
+        {
+            Jump();
+
+
+        }
+    }
+
+    //OnSprintInput but use events 
+
+
+    private IEnumerator testGroundCheck()
+    {
+        repeatGCloop = false;
+        yield return new WaitForSeconds(0.01f);
+        primaryY = this.transform.position.y;
+        yield return new WaitForSeconds(0.01f);
+        secondaryY = this.transform.position.y;
+        if (primaryY == secondaryY)
+        {
+            grounded = true;
+        }
+        else
+        {
+            if (isOnStairs)
+            {
+                grounded = true;
+            }
+            else
+            {
+                grounded = false;
+            }
+
+        }
+        repeatGCloop = true;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag == "Stairs")
+        {
+            //Debug.Log("Colliding with stairs");
+            isOnStairs = true;
+        }
+
+
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.tag == "Stairs")
+        {
+            //Debug.Log("Leaving stairs");
+            isOnStairs = false;
+        }
+        /*if (collision.transform.tag == "Ground")
+        {
+            //Debug.Log("Leaving GROUND");
+        }*/
+
+
+    }
+    private void MovePlayer()
+    {
+
+        //calc movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+            /*if (rb.velocity.magnitude > 0.01f)
+            {
+                anim.SetBool("isWalking", true);
+
+            }
+            else
+            {
+                anim.SetBool("isWalking", false);
+            }*/
+
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+            //anim.SetBool("isWalking", false);
+        }
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        //reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+    }
+
+    void Sprint()
+    {
+        SprintSpeedControl();
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (grounded)
+        {
+            rb.AddForce(moveDirection.normalized * sprintSpeed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * sprintSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
+    }
+
+    private void SprintSpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //limit velocity if needed
+        if (flatVel.magnitude > sprintSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * sprintSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+}
